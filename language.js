d59c75dc7d1654d30300911d8c33c0b7b4cc3aff
Pankaj Pramanik <pkp2.me2k9@gmail.com>
2020-07-03 23:46:59 +0600

    unique id validation added

---
+++ language.js
@@ -0,0 +1,42 @@
+export default {
+
+    'tour.login.button.text': 'Anmeldung',
+    'tour.signUp.button.text': 'Anmelden',
+  
+    // Sign Up Screen
+    'signUp.title.header.text': 'Anmelden',
+    'signUp.encouragement.text.message': 'Du bist fast da! Geben Sie unten Ihre Daten ein, um loszulegen.',
+    'signUp.firstName.input.placeHolder': 'Vorname',
+    'signUp.firstName.input.errorMessage': 'Muss 2-20 zeichen sein',
+    'signUp.lastName.input.placeHolder': 'Familienname, nachname',
+    'signUp.lastName.input.errorMessage': 'Muss 2-20 zeichen sein',
+    'signUp.email.input.placeHolder': 'Email',
+    'signUp.email.input.errorMessage': 'Bitte geben sie eine gültige e-mail-adresse an',
+    'signUp.password.input.placeHolder': 'Passwort',
+    'signUp.password.input.errorMessage': 'Muss aus 8-15 zeichen bestehen und mindestens eine zahl und ein symbol enthalten',
+    'signUp.signUp.button.text': 'Anmelden',
+    'signUp.signUp.button.loadingText': 'Anmeldung',
+  
+    // Login Screen
+    'login.title.header.text': 'Anmeldung',
+    'login.email.input.placeHolder': 'Email',
+    'login.email.input.errorMessage': 'Ungültige Email',
+    'login.password.input.placeHolder': 'Passwort',
+    'login.password.input.errorMessage': 'Ungültige anmeldeinformationen',
+    'login.login.button.text': 'Anmeldung',
+    'login.login.button.loadingText': 'Einloggen',
+    'login.forgotPassword.link.text': 'Passwort Vergessen',
+  
+    // Home Screen
+    'home.title.header.text': 'Zuhause',
+    'home.welcome.subHeader.text': 'Herzlich Willkommen',
+    'home.welcome.paragraph.text': 'Schauen Sie sich Ihren neuen Startbildschirm an! Fühlen Sie sich frei, um ein Stöbern zu haben.',
+  
+    // Settings Screen
+    'settings.title.header.text': 'Die Einstellungen',
+    'settings.firstName.label.text': 'Vorname',
+    'settings.lastName.label.text': 'Familienname, Nachname',
+    'settings.signOut.link.text': 'Ausloggen',
+    'settings.deleteAccount.link.text': 'Konto löschen',
+  };
+  
